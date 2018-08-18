6c4941a3efcbfae521eefbaf19d147fe9bee192a
Steven Paulino <stevenpaulino1@hotmail.com>
2018-08-18 20:49:33 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -34,4 +34,31 @@ def count_contestants_by_hometown(data, hometown)
 		end
 	end
 	count
-end	end
+end	
+
+def get_occupation(data, hometown)
+  occupation = nil
+	data.each do |season_name, girls|
+		girls.each do |girl|
+			if girl["hometown"] == hometown && occupation == nil
+				occupation = girl["occupation"]
+			end
+		end
+	end
+	occupation
+end	
+
+
+def get_average_age_for_season(data, season)
+  	sum = 0
+		count = 0
+	data.each do |season_name, girls|
+		if season_name == season
+			girls.each do |girl|
+				sum += girl["age"].to_i
+				count += 1
+			end
+		end
+	end
+	(sum.to_f / count.to_f).round.to_i
+end 
