88b6adf5014fcd49f20fe9d91f6a7968d2eda5b5
Steven Paulino <stevenpaulino1@hotmail.com>
2018-08-18 20:47:03 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,14 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
-end
+  # code here		name = ""
+	data.each do |season_name, girls|
+		if season_name == season
+			girls.each do |girl|
+				if girl["status"] == "Winner"
+					name = girl["name"].split(" ")[0]
+				end
+			end
+		end
+	end
+	name
+end	
 
-def get_contestant_name(data, occupation)
-  # code here
-end
-
-def count_contestants_by_hometown(data, hometown)
-  # code here
-end
-
-def get_occupation(data, hometown)
-  # code here
-end
-
-def get_average_age_for_season(data, season)
-  # code here
-end
